7a3dcc8fe2fc6388b3088f3584781408eda10068
talkaboutdinner <comrad109@hotmail.com>
2021-10-04 15:41:25 +0800

    testing if merge worked

--- src/Components/Rigidbody.h
+++ src/Components/Rigidbody.h
@@ -1,5 +1,11 @@
 #pragma once
 
+// for my knowledge only : jy -> transform shall store bounding shape
+//                                  rigidbody shall store the velocity
+
+#include "Physics/PhysicsMaterial.h"
+#include "Math/Vector3f.h"
+
 struct rigidbody
 {
 	constexpr static auto typedef_v = paperback::component::type::data
@@ -7,8 +13,28 @@ struct rigidbody
         .m_pName = "Rigidbody"
     };
 
+    xcore::vector3		m_Accel;
 	xcore::vector3		m_Velocity;
-	float				m_Mass;
+	float				    m_Mass;
+    //float _fColliderWidth;									// -- Width of bounding box
+    //float _fColliderHeight;									// -- Height of bounding box
+    //PhysicMaterial _Material;								// -- Material of the Entity
+    //float _fInitWidth = -1.f, _fInitHeight = -1.f;			// -- Initial width and height
+    //float _fMass;											// -- Mass value of the object
+    //float _fInvMass;										// -- Inverse Mass value of the object
+    //float _fRestituition;									// -- Elasticity, bounce factor for velocity when colliding
+    //float _fFriction = 1.f;									// -- Friction, defaulted to 1.f
+    //float _fDrag;											// -- Drag (forces)
+    //paperback::Vector3f _vAccel;							// -- Acceleration of rigidbody
+    //paperback::Vector3f _vSumofForces;						// -- Sum of Forces of rigidbody
+    //float _fRadius;											// -- Radius of the object
+    //int _iCollisionFlag = 0;								// -- Collision flag for the rigidbody
+    //int _iMapCollisionFlag = 0;								// -- Collision flag for environments
+    //rigidbody* _pCollision = nullptr;						// -- Are there any collisions active on this rb
+    //void AddForce(const paperback::Vector3f& force);		// -- Applies a force
+    //void AddImpulse(const paperback::Vector3f impulse);		// -- Applies an impulse
+    //void ApplyForces(float dt);								// -- Apply all forces
+    //void ResetForces();										// -- Resets all forces
 };
 
 namespace RR_Rigidbody
