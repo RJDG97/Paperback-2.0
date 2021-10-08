a47464cc54c91c93e42f29f6b7f09876f80d491e
l-junpu <57226077+l-junpu@users.noreply.github.com>
2021-10-08 18:29:07 +0800

    partial mem fix

--- src/Systems/ImguiSystem.h
+++ src/Systems/ImguiSystem.h
@@ -509,7 +509,7 @@ struct imgui_system : paperback::system::instance
 
 
     PPB_INLINE
-        void Terminated(void) noexcept
+        void OnSystemTerminated(void) noexcept
     {
         ImGui_ImplOpenGL3_Shutdown();
         ImGui_ImplGlfw_Shutdown();
