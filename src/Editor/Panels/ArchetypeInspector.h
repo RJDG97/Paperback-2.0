459c5966db1f04ab178a08c6c45880a2a317abda
Adele <eelingadele.sim@digipen.edu>
2021-10-15 19:52:39 +0800

    Deletion Now works
    - There is a 16 byte memleak

--- src/Editor/Panels/ArchetypeInspector.h
+++ src/Editor/Panels/ArchetypeInspector.h
@@ -3,6 +3,9 @@
 
 struct ArchetypeInspector : paperback::editor::instance
 {
+    paperback::archetype::instance* m_pArchetype; //refers back to the archetype that the entity is referencing to
+    std::vector <const char*> m_ComponentNames = {};
+
     constexpr static auto typedef_v = paperback::editor::type::update
     {
         .m_pName = "Archetypes/Prefabs"
@@ -11,12 +14,72 @@ struct ArchetypeInspector : paperback::editor::instance
     void OnSystemCreated(void) noexcept
     {
         m_bEnabled = true;
+        m_pArchetype = nullptr;
     }
 
     void Update(void) noexcept
     {
+        int Index = 0;
+        std::string ArchetypeName;
+
         ImGui::Begin(ArchetypeInspector::typedef_v.m_pName);
 
+        static ImGuiTextFilter Filter;
+        Filter.Draw(ICON_FA_FILTER, 150.0f);
+
+
+        ImGui::BeginChild("Archetypes: ", { ImGui::GetContentRegionAvailWidth() / 2, ImGui::GetContentRegionAvail().y }, true);
+
+        for (auto& Archetype : PPB.GetArchetypeList())
+        {
+            ++Index;
+
+            char Buffer[256]{};
+            ArchetypeName = Archetype->GetName();
+            std::copy(ArchetypeName.begin(), ArchetypeName.end(), Buffer);
+
+            if (Filter.PassFilter(ArchetypeName.c_str()))
+            {
+                ImGui::PushItemWidth( 150.0f );
+
+                if (ImGui::InputText(("##ArchetypeName" + std::to_string(Index)).c_str(), Buffer, IM_ARRAYSIZE(Buffer), ImGuiInputTextFlags_EnterReturnsTrue))
+                    Archetype->SetName(std::string(Buffer));
+
+                ImGui::PopItemWidth();
+
+                if ( ImGui::IsItemClicked() )
+                {
+                    m_pArchetype = Archetype;
+                    m_ComponentNames.clear();
+
+                    for ( paperback::u32 i = 0; i < Archetype->GetComponentNumber(); ++i )
+                    {
+                        m_ComponentNames.push_back( Archetype->GetComponentInfos()[i]->m_pName );
+                    }
+                }
+            }
+        }
+        ImGui::EndChild();
+
+        ImGui::SameLine();
+
+        ImGui::BeginChild("Details: ");
+
+        if ( !m_ComponentNames.empty() && m_pArchetype )
+        {
+            ImGui::Text( "Number of Entities: %d", m_pArchetype->GetEntityCount() );
+
+            if ( ImGui::Button("Spawn New Entity") )
+                m_pArchetype->CreateEntity();
+
+            ImGui::Separator();
+
+            ImGui::Text( "Archetype Components: " );
+
+            for (auto& Names : m_ComponentNames)
+                ImGui::Text(Names);
+        }
+        ImGui::EndChild();
 
         ImGui::End();
     }
--- src/Editor/Panels/EntityInspector_Inline.h
+++ src/Editor/Panels/EntityInspector_Inline.h
@@ -4,6 +4,7 @@ void EntityInspector::InspectorWindow()
 {
     int NumEntities = 0, Index = 0;
     bool b_NodeOpen = false;
+    std::stringstream Label;
 
     ImGui::Begin(EntityInspector::typedef_v.m_pName);
 
@@ -22,25 +23,43 @@ void EntityInspector::InspectorWindow()
                 {
                     NumEntities++;
 
-                    if (ImGui::Selectable((Archetype->GetName() + " [" + std::to_string(i) + "]").c_str() ))
+                    ImGuiTreeNodeFlags NodeFlags = ((m_Imgui.m_SelectedEntity.first == Archetype && m_Imgui.m_SelectedEntity.second == i) ? ImGuiTreeNodeFlags_Selected : 0) | ImGuiTreeNodeFlags_OpenOnArrow; //update this
+                    NodeFlags |= ImGuiTreeNodeFlags_SpanAvailWidth | ImGuiTreeNodeFlags_Leaf;
+
+                    b_NodeOpen = ImGui::TreeNodeEx((char*)("##" + Archetype->GetName() + " [" + std::to_string(i) + std::to_string(Index) + "]").c_str(), NodeFlags, (Archetype->GetName() + " [" + std::to_string(i) + "]").c_str());
+
+                    if (ImGui::IsItemClicked())
                     {
                         m_Imgui.m_SelectedEntity.first = Archetype;
                         m_Imgui.m_SelectedEntity.second = i;
                         m_Imgui.m_Components = Archetype->GetEntityComponents(m_Imgui.m_SelectedEntity.second);
-
                     }
 
-                    ImGui::SameLine();
+                    bool Deleted = false;
 
-                    if (ImGui::Button(ICON_FA_TRASH))
+                    if (ImGui::BeginPopupContextItem())
                     {
-                        ImGui::OpenPopup(ICON_FA_TRASH " Delete?");
+                        if (ImGui::MenuItem(ICON_FA_TRASH "Delete?"))
+                        {
+                            m_Imgui.m_SelectedEntity.first = Archetype;
+                            m_Imgui.m_SelectedEntity.second = i;
+                            Label << m_Imgui.m_SelectedEntity.first->GetName() << " [" << std::to_string(i) << "]";
+                            Deleted = true;
+                        }
 
-                        DeleteEntity(ICON_FA_TRASH " Delete?", i, (Archetype->GetName() + " [" + std::to_string(i) + "]"));
+                        ImGui::EndPopup();
                     }
+
+
+                    if (b_NodeOpen) ImGui::TreePop();
+
+                    if ( Deleted )
+                        ImGui::OpenPopup(ICON_FA_TRASH " Delete?");
                 }
             }
         }
+
+        DeleteEntity(ICON_FA_TRASH " Delete?", m_Imgui.m_SelectedEntity.second, Label.str());
     }
 
     ImGui::Separator();
