2db945e812a18aa38a25b81dcbae52a33827fa53
Renzo Garcia <renzo_garcia@live.com>
2021-12-03 16:57:29 +0800

    Units Attack Bases now

--- build/Sandbox/Systems/OnEvent_CollisionSystem.h
+++ build/Sandbox/Systems/OnEvent_CollisionSystem.h
@@ -106,11 +106,17 @@ struct onevent_UnitTriggerStay_system : paperback::system::instance
         auto m_obj2 = GetEntityInfo(obj2.m_GlobalIndex);
 
         // Get Relevant Components
-        auto [ Unit_1_Friendly, Unit_1_Enemy, Unit_State ] = m_obj.m_pArchetype->FindComponents< friendly, enemy, unitstate >( m_obj.m_PoolDetails );
-        auto [ Unit_2_Friendly, Unit_2_Enemy, Unit_State2 ] = m_obj2.m_pArchetype->FindComponents< friendly, enemy, unitstate >( m_obj2.m_PoolDetails );
+        auto [ Unit_1_Friendly, Unit_1_Enemy, Unit_State, Base_1 ] = m_obj.m_pArchetype->FindComponents< friendly, enemy, unitstate, base >( m_obj.m_PoolDetails );
+        auto [ Unit_2_Friendly, Unit_2_Enemy, Unit_State2, Base_2 ] = m_obj2.m_pArchetype->FindComponents< friendly, enemy, unitstate, base >( m_obj2.m_PoolDetails );
+
+        if (Base_1) {
+            // Disable Movement - Maintain Collision
+            ResetForces(rf, rf2);
+            return;
+        }
 
         // Different Unit Types - ATTACK
-        if ( Unit_1_Friendly && Unit_2_Enemy || Unit_1_Enemy && Unit_2_Friendly )
+        if ( Unit_State && Unit_1_Friendly && Unit_2_Enemy || Unit_1_Enemy && Unit_2_Friendly )
         {
             // Disable Movement - Maintain Collision
             ResetForces( rf, rf2 );
@@ -133,7 +139,7 @@ struct onevent_UnitTriggerStay_system : paperback::system::instance
                 auto [ Health_2 ]          = m_obj2.m_pArchetype->FindComponents< health >( m_obj2.m_PoolDetails );
 
                 // Update Unit Health
-                if ( Damage_1 && Health_2 && Timer_1 )
+                if ( Damage_1 && Health_2 && (Timer_1 || Base_2) )
                 {
                     if ( Timer_1->m_Value <= 0.0f )
                     {
@@ -141,7 +147,7 @@ struct onevent_UnitTriggerStay_system : paperback::system::instance
                         Health_2->m_CurrentHealth -= Damage_1->m_Value;
 
                         // Delete Entity
-                        if ( Health_2->m_CurrentHealth <= 0 )
+                        if (!Base_2 && Health_2->m_CurrentHealth <= 0 )
                         {
                             DeleteEntity( obj2 );
                             Unit_State->SetState( UnitState::WALK );
@@ -162,7 +168,7 @@ struct onevent_UnitTriggerStay_system : paperback::system::instance
             ResetForces( rf, rf2 );
 
             // Set to IDLE
-            if ( Unit_State->IsNotState( UnitState::ATTACK ) &&
+            if (Unit_State && Unit_State2 && Unit_State->IsNotState( UnitState::ATTACK ) &&
                  Unit_State2->IsState( UnitState::ATTACK ) )
             {
                 // Helps to Re-Enable movement when SAME Unit Types collide - This also causes the units to overshot
