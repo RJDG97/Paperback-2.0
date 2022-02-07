83ec09f78e5fa153726e443ca1ea10ec5fc9857e
WhenisDinner <57213880+WhenisDinner@users.noreply.github.com>
2022-02-07 11:32:39 +0800

    Stopped elevators in section 3

--- build/CSScript/MovingPlatformSwitch.cs
+++ build/CSScript/MovingPlatformSwitch.cs
@@ -44,7 +44,6 @@ namespace CSScript
         {
             if (m_ChildID != -1 && (ID == Player.GetJumpUnitID() || ID == Player.GetPushUnitID()) /*|| collision with blocks*/)
             {
-                Debug.Log("henlorp");
                 m_ChildPathFollower.m_PauseTravel = false;
                 m_ChildPathFollower.m_Reversed = false;
             }
