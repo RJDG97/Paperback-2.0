e04156c6a605b6d5dba7c095f802054d9dd3b801
Renzo Garcia <renzo_garcia@live.com>
2022-04-05 00:49:13 +0800

    Updated Cutscene Win Colliders and lighting

--- build/CSScript/Goal_Cutscene3.cs
+++ build/CSScript/Goal_Cutscene3.cs
@@ -35,10 +35,10 @@ namespace CSScript
 
         public void OnCollisionEnter(UInt32 ID)
         {
-            if ((ID == Player.GetJumpUnitID() || ID == Player.GetPushUnitID() /*|| collision with blocks*/))
-            {
+            //if ((ID == Player.GetJumpUnitID() || ID == Player.GetPushUnitID() /*|| collision with blocks*/))
+            //{
                 Application.ChangeScene("Credits");
-            }
+            //}
         }
         public void OnCollisionStay(UInt32 ID)
         {
