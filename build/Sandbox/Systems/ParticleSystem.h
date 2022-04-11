ad201e627c47f8e506fb94465f8ccf53b91d9163
WhenisDinner <57213880+WhenisDinner@users.noreply.github.com>
2022-04-12 03:31:25 +0800

    Fixed rotation bug for particles i think

--- build/Sandbox/Systems/ParticleSystem.h
+++ build/Sandbox/Systems/ParticleSystem.h
@@ -55,7 +55,7 @@ struct particle_system : paperback::system::pausable_instance
         {
             if ( Controller.m_PlayerStatus && Camera.m_Active )
             {
-                ActiveCameraPosition = Camera.m_Position;
+                ActiveCameraPosition = glm::cross(Camera.m_Up, Camera.m_Right);
             }
         });
 
@@ -76,7 +76,7 @@ struct particle_system : paperback::system::pausable_instance
             if ( !Emitter.IsAlive() ) return;
 
             bool  ComputedAngle = false;
-            float Angle{};
+            paperback::Vector3f Angle{};
             auto  RequestCount  = Emitter.UpdateEmitter( Dt );
 
             if ( RequestCount && Emitter.IsAlive() )
@@ -96,13 +96,13 @@ struct particle_system : paperback::system::pausable_instance
                 {
                     if ( !ComputedAngle && Debug )
                     {
-                        Angle = Debug->DirtyRotationAnglesFromDirectionalVec( Transform.m_Position - paperback::Vector3f(ActiveCameraPosition.x, ActiveCameraPosition.y, ActiveCameraPosition.z) ).y;
+                        Angle = Debug->DirtyRotationAnglesFromDirectionalVec( paperback::Vector3f(ActiveCameraPosition.x, ActiveCameraPosition.y, ActiveCameraPosition.z) );
                         ComputedAngle = true;
                     }
                     
                     if ( ComputedAngle )
                     {
-                        Rotation.m_Value.y = Angle;
+                        Rotation.m_Value = Angle;
                     }
                 }
             });
