b95b08f7949dd96bf1f2f889e63ac7035d744eb6
k_ded <eelingadele.sim@digipen.edu>
2022-04-13 18:07:19 +0800

    Commented Sandbox PCH files

--- build/Sandbox/PCH/sandbox_pch.h
+++ build/Sandbox/PCH/sandbox_pch.h
@@ -1,3 +1,13 @@
+/**********************************************************************************
+*\file         sandbox_pch.h
+*\brief        sandbox_pch.h
+*
+*\author       Lee Jun Pu, 100% Code Contribution
+*
+*\copyright    Copyright (c) 2022 DigiPen Institute of Technology. Reproduction
+			   or disclosure of this file or its contents without the prior
+			   written consent of DigiPen Institute of Technology is prohibited.
+**********************************************************************************/
 #pragma once
 #ifndef PAPERBACK_PCH_H
 #define PAPERBACK_PCH_H
